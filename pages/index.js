060f2a0c4ba46f4acaf519d6be0cd5aa00a67284
Jasper Mayone <jasper@jaspermayone.com>
2023-01-10 19:23:51 -0500

    :sparkles: and more

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,8 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          ⚠️ Site Coming Soon ⚠️
+        We&rsquo;ll be back soon!
         </h1>
-        <h1>We&rsquo;ll be back soon!</h1>
     <div>
         <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
         <p>&mdash; The Team</p>
