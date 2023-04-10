5d5c194334f44cacec132ae7cefac541beae5ca7
Jasper Mayone <jasper.mayone@singlefeather.com>
2023-04-10 19:26:09 -0400

    :wrench: Change to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<header>
+	<title>Single Feather | Under Construction</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+</header>
+<main>
+	<h1>🚧 Site Under Construction... 🚧</h1>
+	<img
+		src="https://s3.singlefeather.com/cdn/gifs/minions-under-construction.gif"
+		alt="Minions Engineering GIF"
+		width={500}
+		height={277}
+	/>
+	<div>
+		<p>
+			Sorry for the inconvenience but we&rsquo;re curently redesigning our website! If you need to
+			you can always <a href="mailto:webmaster@singlefeather.com">contact us</a>.
+		</p>
+		<p>~ The Web & Development Team @ Single Feather LLC</p>
+	</div>
+	<footer>© 2023 Single Feather LLC ©</footer>
+</main>
+
+<style>
+	main {
+		color: black;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+		background: #f09819; /* fallback for old browsers */
+		background: -webkit-linear-gradient(
+			to right,
+			#edde5d,
+			#f09819
+		); /* Chrome 10-25, Safari 5.1-6 */
+		background: linear-gradient(
+			to right,
+			#edde5d,
+			#f09819
+		); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+		font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
+		font-size: 1rem;
+		text-align: center;
+	}
+
+	footer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+		font-size: 0.9rem;
+		margin-top: 15%;
+		bottom: -5rem;
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotate(360deg);
+		}
+		to {
+			transform: rotate(0deg);
+		}
+	}
+</style>
