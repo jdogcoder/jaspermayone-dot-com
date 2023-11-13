b3bf2895ebf0f6a6335395ec56fe3d03d37006eb
Jasper Mayone <jasper.mayone@singlefeather.com>
2023-11-12 20:53:40 -0500

    fix build errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 
 import ThumbnailView from "@/components/ThumbnailView";
 // import Footer from "@/components/Footer";
-import styles from '@/styles/Home.module.css'
-
+import styles from "@/styles/Home.module.css";
 
 // const PhantomSansFont = localFont({
 //   src: "../public/fonts/Phantom_Sans_0.7/Book.woff2",
@@ -44,31 +43,48 @@ export default function Home() {
                 }}
               />
             </div>
-            <div className="column is-12" style={{
-              marginLeft: "150px",
-              marginTop: "5px",
-              marginRight: "-100px",
-            }}>
-                <h1 className="title is-1" >👋 Hey there! I'm Jasper.</h1>
-                <h2 className="subtitle"></h2>
-                <p>
-                <br/>
-                  I’m Jasper (jdogcoder on the internet)! I use the he/him/his pronouns, and am a 17 year old based out of the best state ever, Vermont!
-                  <br/><br/>
-                  I love coding, cybersecurity, my cats, and am also an Aerial Silks artist! (it’s a circus thing)
-                  <br/><br/>
-                  I'm currently working on my startup Cyaneus (a privacy based social media company), among many other cool things and side projects!
-                  <br/><br/>
-                  I am fluent with the coding languages Typescript (and javascript), Svelte, and Express, and am working to fluency with Java and Python!
-                  </p>
+            <div
+              className="column is-12"
+              style={{
+                marginLeft: "150px",
+                marginTop: "5px",
+                marginRight: "-100px",
+              }}
+            >
+              <h1 className="title is-1">{"👋 Hey there! I'm Jasper."}</h1>
+              <h2 className="subtitle"></h2>
+              <p>
+                <br />
+                {
+                  "I’m Jasper (jdogcoder on the internet)! I use the he/him/his pronouns, and am a 17 year old based out of the best state ever, Vermont!"
+                }
+                <br />
+                <br />
+                {
+                  "I love coding, cybersecurity, my cats, and am also an Aerial Silks artist! (it’s a circus thing)"
+                }
+                <br />
+                <br />
+                {
+                  "I'm currently working on my startup Cyaneus (a privacy based social media company), among many other cool things and side projects!"
+                }
+                <br />
+                <br />
+                {
+                  "I am fluent with the coding languages Typescript (and javascript), Svelte, and Express, and am working to fluency with Java and Python!"
+                }
+              </p>
             </div>
           </div>
           <div>
-              <p>Feel free to check out the other tabs in the menus above to see some of the things I have done & worked on!</p>
-            </div>
+            <p>
+              {
+                "Feel free to check out the other tabs in the menus above to see some of the things I have done & worked on!"
+              }
+            </p>
+          </div>
         </div>
       </main>
-
     </>
   );
 }
